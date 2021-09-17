92d45ed6074ac1051894ffbf29ea3f54cf67e852
MAgungHKM <maranga12341@yahoo.com>
2021-09-17 13:25:20 +0700

    Initialized Global styling

---
+++ src/styles/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export const MainStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+});
